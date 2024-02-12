fcf8ff1538bb1dc4b89bfe228400e049b9559007
Guille <guillermobernallou@gmail.com>
2024-02-12 10:27:26 +0100

    Update global styles and import Live component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import { Room } from "./Room";
+"use client";
+
+import Live from "@/components/Live";
 
 export default function Page() {
   return (
-    <h1>Live Blocks In Figma</h1>
+    <div>
+      <Live />
+    </div>
   );
 }
